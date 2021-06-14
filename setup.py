25f8d1dcb68269266aeef8170aaa2565c3e7b16a
Simon Honigmann <shonigmann@blueorigin.com>
2021-06-14 15:05:43 -0700

    fixed package setup

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 from setuptools import setup
 
-PACKAGE_NAME = 'ros2_xacro_gazebo_spawner'
+package_name = 'ros2_xacro_gazebo_spawner'
 
 setup(
-    name=PACKAGE_NAME,
+    name=package_name,
     version='0.0.1',
-    packages=[PACKAGE_NAME],
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + PACKAGE_NAME]),
-        ('share/' + PACKAGE_NAME, ['package.xml']),
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
+    maintainer='Simon Honigmann',
+    maintainer_email='simonhonigmann@gmail.com',
+    description='A simple package that can dynamically spawn robots defined in xacro into Gazebo from ROS2 launch files',
+    license='Apache 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'ros2_xacro_gazebo_spawner = ros2_xacro_gazebo_spawner.ros2_xacro_gazebo_spawner:main',
         ],
     },
 )
