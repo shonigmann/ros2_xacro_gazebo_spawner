ddebcec3bc6a171eaee6fcf4be1df44a09a6250b
Simon Honigmann <shonigmann@blueorigin.com>
2021-07-12 16:16:56 -0700

    modified setup.py

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'ros2_xacro_gazebo_spawner = ros2_xacro_gazebo_spawner.ros2_xacro_gazebo_spawner:main',
         ],
     },
 )
